bdd33e37a1dc3a3ef8a7ae1d941a19f1bb2727af
Alan 21 Code <alan21naci@gmail.com>
2020-08-10 02:18:10 -0300

    Le agregue color al App, Banner y Creator

--- src/components/TaskBanner.js
+++ src/components/TaskBanner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const TaskBanner = (props) => (
   <h4 className="bg-warning text-darck text-center p-4">
-    {props.userName}Aplicación de tareas. (
+    {props.userName}Aplicación de Tareas. (
     {props.taskItems.filter((t) => !t.done).length} tareas por hacer)
   </h4>
 );
